956693b8aeb52d158896ae5e877ebb3ef8d693eb
W1ZARD750 <davmog08@gmail.com>
2023-01-13 17:28:11 +0200

    just add remarks in English to my task(gripper)
    all I need to do I do
    just in future we need put correct number of open and close servos position
    and thats all

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gripper.java
@@ -9,7 +9,7 @@ public class gripper
     private HardwareMap _hardwaremap;
     private double _openServoPos = 0;
     private double _closeServoPos = 0;
-    //בשלב עשינו משתנים ולכמה מיהם נתנו ערכים
+    //in this part I write changes and for some of them I add values
 
     public gripper(HardwareMap hardwaremap)
     {
@@ -18,18 +18,18 @@ public class gripper
         _right = hardwaremap.get(Servo.class, "gripper_servo_right");
         _left.setDirection(Servo.Direction.REVERSE);
         openGripper();
-    //בשלב זה יצרנו פונקציה שאחראית על לתת למשתנים
+    //in this part I make fuction that change left servo direction,call other fuction and etc
     }
     public void openGripper()
     {
         _left.setPosition(_openServoPos);
         _right.setPosition(_openServoPos);
-    //
+    //in this part I make fuction that open servos
     }
     public void closeGripper()
     {
         _left.setPosition(_closeServoPos);
         _right.setPosition(_closeServoPos);
-    // בשלב זה יצרנו עוד פונקציה שאחראית על סגירת הסרבואים
+    // in this part I make fuction that close servos
     }
 }
