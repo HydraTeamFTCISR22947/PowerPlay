88a306eef270aba432a09a7ee4458ea1d8aa73b8
HAL9000AI <alex.klinkovich2@gmail.com>
2023-01-30 20:03:24 +0200

    MeepMeep and Autonomous
    Completed the MeepMeep and Autonomous for each postion(redleft,blueleft, etc...)
    added new Pose2d for the robot to move closer to the cone stack each cycle
    * check the angles irl
    * check the cone offset(movement to stack of cones each cycle) irl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoLeftRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoLeftRed.java
@@ -12,26 +12,30 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
 
-@Autonomous(name = "Auto Left Red Test",group = "auto")
+@Autonomous(name = "Auto Left Red",group = "auto")
 public class AutoLeftRed extends LinearOpMode
 {
     //start values.
     public static final double START_POSE_X = -36, START_POSE_Y = -72, START_POSE_ANGLE = 90;
 
-    /* NOT FINAL*/
+    /* NOT FINAL */
     public static final double START_CONE_DELIVERY_X = -36, START_CONE_DELIVERY_Y = 0, START_CONE_DELIVERY_ANGLE = 0;
-    public static final double CONE_DELIVERY_POSE_X = -32, CONE_DELIVERY_POSE_Y = -12, CONE_DELIVERY_POSE_ANGLE = 30;
+    public static final double CONE_DELIVERY_POSE_X = -32, CONE_DELIVERY_POSE_Y = -12, CONE_DELIVERY_POSE_ANGLE = 53;
     public static final double CONE_INTAKE_POSE_X = -56, CONE_INTAKE_POSE_Y = -12, CONE_INTAKE_POSE_ANGLE = 0;
-    //Need to put actual coordiantes
+    //Need to put actual coordinates
     public static final double  PARK_POSE_X = 0, PARK_POSE_Y = 0, PARK_POSE_ANGLE = 180;
 
     public static final double TRACK_WIDTH = 13.23;
     public static final double MAX_VELOCITY = 80;
     public static final double MAX_ACCELERATION = 60;
     public static final double MAX_ANGLE_VELOCITY = 180;
-    public static final double MAX_ANGLE_ACCELARATION = 180;
+    public static final double MAX_ANGLE_ACCELERATION = 180;
 
-    public static  final long DELIVERY_WAIT_TIME = 2, INTAKE_WAIT_TIME = 4;
+    public static final double DELIVERY_WAIT_TIME = 2, INTAKE_WAIT_TIME = 4;
+    public static final double CONE_INTAKE_OFFSET = 1;// move closer to cone stack each cycle ( check this irl )
+
+
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -45,28 +49,68 @@ public class AutoLeftRed extends LinearOpMode
         Pose2d firstCycleBarPose = new Pose2d(START_CONE_DELIVERY_X, START_CONE_DELIVERY_Y, Math.toRadians(START_CONE_DELIVERY_ANGLE));
         Pose2d secondCycleBarPose = new Pose2d(CONE_DELIVERY_POSE_X, CONE_DELIVERY_POSE_Y, Math.toRadians(CONE_DELIVERY_POSE_ANGLE));
         Pose2d coneStackPose = new Pose2d(CONE_INTAKE_POSE_X, CONE_INTAKE_POSE_Y, Math.toRadians(CONE_INTAKE_POSE_ANGLE));
+        Pose2d coneStackPoseSecond = new Pose2d(CONE_INTAKE_POSE_X-CONE_INTAKE_OFFSET,CONE_INTAKE_POSE_Y,Math.toRadians(CONE_INTAKE_POSE_ANGLE));
+        Pose2d coneStackPoseThird = new Pose2d(CONE_INTAKE_POSE_X-2*CONE_INTAKE_OFFSET,CONE_INTAKE_POSE_Y,Math.toRadians(CONE_INTAKE_POSE_ANGLE));
+
 
         //  Pose2d parkPose = new Pose2d(parkX,parkY, Math.toRadians(parkAngle));
 
         TrajectorySequence autoCycles = drivetrain.trajectorySequenceBuilder(startPose)
+
                 /* First Cycle*/
                 .lineToLinearHeading(firstCycleBarPose)//delivery
                 .waitSeconds(DELIVERY_WAIT_TIME)
 
+                /*Second Cycle*/
                 .splineToLinearHeading(coneStackPose,Math.toRadians(START_POSE_ANGLE))//intake
                 .waitSeconds(INTAKE_WAIT_TIME)
 
-                /*Second Cycle*/
                 .lineToLinearHeading(secondCycleBarPose)//delivery
                 .waitSeconds(DELIVERY_WAIT_TIME)
 
-                .lineToLinearHeading(coneStackPose)//intake
+                /*Third Cycle*/
+                .lineToLinearHeading(coneStackPoseSecond)//intake
                 .waitSeconds(INTAKE_WAIT_TIME)
 
                 .lineToLinearHeading(secondCycleBarPose)//delivery
+                .waitSeconds(DELIVERY_WAIT_TIME)
+
+                /*Fourth Cycle*/
+                .lineToLinearHeading(coneStackPoseThird)//intake
+                .waitSeconds(INTAKE_WAIT_TIME)
+
+                .lineToLinearHeading(secondCycleBarPose)//delivery
+                .waitSeconds(DELIVERY_WAIT_TIME)
+
+
                 .build();
 
-        if(isStopRequested())
+       /* Trajectory firstCycleDelivery = drivetrain.trajectoryBuilder(startPose)
+                .lineToLinearHeading(firstCycleBarPose)
+                .build();
+
+        Trajectory firstCycleIntake = drivetrain.trajectoryBuilder(firstCycleDelivery.end())
+                .splineToLinearHeading(coneStackPose,Math.toRadians(START_POSE_ANGLE))
+                .build();
+
+        Trajectory secondCycleDelivery = drivetrain.trajectoryBuilder(firstCycleIntake.end())
+                .lineToLinearHeading(secondCycleBarPose)
+                .build();
+
+        Trajectory secondCycleIntake = drivetrain.trajectoryBuilder(secondCycleDelivery.end())
+                .lineToLinearHeading(coneStackPose)
+                .build();
+
+        Trajectory thirdCycleDelivery = drivetrain.trajectoryBuilder(secondCycleIntake.end())
+                .lineToLinearHeading(secondCycleBarPose)
+                .build();*/
+
+/*
+            Trajectory park = drivetrain.trajectoryBuilder(secondCycleScore.end())
+                    .splineToLinearHeading(parkPose,parkAngle)
+                    .build();
+*/
+        if (isStopRequested())
         {
             return;
         }
