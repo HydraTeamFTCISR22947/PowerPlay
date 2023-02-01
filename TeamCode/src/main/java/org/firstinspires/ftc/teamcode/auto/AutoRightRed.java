b56d1b7698f68a99875bfe7fc8db623e067c6194
Ariel Gabay <ariegabay1@gmail.com>
2023-02-01 21:28:36 +0200

    AutoBlueRightUpdated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoRightRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoRightRed.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.MarkerCallback;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,11 +11,16 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.subsystems.ElevatorSystem;
+import org.firstinspires.ftc.teamcode.subsystems.TransferSystem;
+
 
 @Config
 @Autonomous(name = "Auto Right Red", group = "auto")
 public class AutoRightRed extends LinearOpMode {
 
+    ElevatorSystem elevator = new ElevatorSystem(hardwareMap);
+    TransferSystem transferSystem;
     //start values.
     public static final double START_POSE_X = 36, START_POSE_Y = -72, START_POSE_ANGLE = 90;
 
@@ -35,56 +41,52 @@ public class AutoRightRed extends LinearOpMode {
         Pose2d startPose = new Pose2d(START_POSE_X, START_POSE_Y, Math.toRadians(START_POSE_ANGLE));
         drivetrain.setPoseEstimate(startPose);
 
-        Pose2d coneStackPose1 = new Pose2d(56,-12,Math.toRadians(180));
+        Pose2d coneStackPose1 = new Pose2d(56, -12, Math.toRadians(180));
         Pose2d firstCycleBarPose = new Pose2d(START_CONE_DELIVERY_X, START_CONE_DELIVERY_Y, Math.toRadians(START_CONE_DELIVERY_ANGLE));
         Pose2d secondCycleBarPose = new Pose2d(CONE_DELIVERY_POSE_X, CONE_DELIVERY_POSE_Y, Math.toRadians(CONE_DELIVERY_POSE_ANGLE));
         Pose2d coneStackPose = new Pose2d(CONE_INTAKE_POSE_X, CONE_INTAKE_POSE_Y, Math.toRadians(CONE_INTAKE_POSE_ANGLE));
-        Pose2d coneStackPoseSecond = new Pose2d(CONE_INTAKE_POSE_X+INTAKE_OFFSET,CONE_INTAKE_POSE_Y,Math.toRadians(CONE_INTAKE_POSE_ANGLE));
-        Pose2d coneStackPoseThird = new Pose2d(CONE_INTAKE_POSE_X+2*INTAKE_OFFSET,CONE_INTAKE_POSE_Y,Math.toRadians(CONE_INTAKE_POSE_ANGLE));
+        Pose2d coneStackPoseSecond = new Pose2d(CONE_INTAKE_POSE_X + INTAKE_OFFSET, CONE_INTAKE_POSE_Y, Math.toRadians(CONE_INTAKE_POSE_ANGLE));
+        Pose2d coneStackPoseThird = new Pose2d(CONE_INTAKE_POSE_X + 2 * INTAKE_OFFSET, CONE_INTAKE_POSE_Y, Math.toRadians(CONE_INTAKE_POSE_ANGLE));
+
+        MarkerCallback ElevatorMax = new MarkerCallback() {
+            @Override
+            public void onMarkerReached() {
+
+                elevator.highRod();
+            }
 
 
+            TrajectorySequence autoCycles = drivetrain.trajectorySequenceBuilder(startPose)
 
-        TrajectorySequence autoCycles = drivetrain.trajectorySequenceBuilder(startPose)
+                    /* First Cycle*/
+                    .lineToLinearHeading(firstCycleBarPose)//delivery
+                    .waitSeconds(DELIVERY_WAIT_TIME)
 
-                /* First Cycle*/
-                .lineToLinearHeading(firstCycleBarPose)//delivery
-                .waitSeconds(DELIVERY_WAIT_TIME)
+                    /*Second Cycle*/
+                    //   .splineToLinearHeading(coneStackPose,Math.toRadians(200))//intake
 
-                /*Second Cycle*/
-             //   .splineToLinearHeading(coneStackPose,Math.toRadians(200))//intake
-
-                .lineToSplineHeading(coneStackPose1)
-                .splineToLinearHeading(coneStackPose,Math.toRadians(START_POSE_ANGLE))//intake
+                    .lineToSplineHeading(coneStackPose1)
+                    .splineToLinearHeading(coneStackPose, Math.toRadians(START_POSE_ANGLE))//intake
 
 //                .lineToSplineHeading(coneStackPose)
-                .waitSeconds(INTAKE_WAIT_TIME)
+                    .waitSeconds(INTAKE_WAIT_TIME)
 
-                .lineToLinearHeading(secondCycleBarPose)//delivery
-                .waitSeconds(DELIVERY_WAIT_TIME)
+                    .lineToLinearHeading(secondCycleBarPose)//delivery
+                    .waitSeconds(DELIVERY_WAIT_TIME)
 
-                /*Third Cycle*/
-                .splineToLinearHeading(coneStackPose1,Math.toRadians(START_POSE_ANGLE))//intake
-                .lineToSplineHeading(coneStackPose)
+                    /*Third Cycle*/
+                    .splineToLinearHeading(coneStackPose1, Math.toRadians(START_POSE_ANGLE))//intake
+                    .lineToSplineHeading(coneStackPose)
 
 //                .lineToLinearHeading(coneStackPoseSecond)//intake
-                .waitSeconds(INTAKE_WAIT_TIME)
+                    .waitSeconds(INTAKE_WAIT_TIME)
 
-                .lineToLinearHeading(secondCycleBarPose)//delivery
-                .waitSeconds(DELIVERY_WAIT_TIME)
+                    .lineToLinearHeading(secondCycleBarPose)//delivery
+                    .waitSeconds(DELIVERY_WAIT_TIME)
 
-                .build();
-
-        if (isStopRequested())
-        {
-            return;
-        }
-        waitForStart();
-        while(opModeIsActive())
-        {
-            drivetrain.followTrajectorySequence(autoCycles);
-        }
-
+                    .build();
+        };
     }
-
-
 }
+
+
