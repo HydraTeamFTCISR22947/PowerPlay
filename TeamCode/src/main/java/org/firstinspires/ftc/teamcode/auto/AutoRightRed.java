a29717d411ff44b1bc1c38eec48c35a37cd0b75b
Ariel Gabay <ariegabay1@gmail.com>
2023-02-02 13:35:22 +0200

    Added some changes to red right auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoRightRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoRightRed.java
@@ -11,23 +11,25 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
-import org.firstinspires.ftc.teamcode.subsystems.ElevatorSystem;
-import org.firstinspires.ftc.teamcode.subsystems.TransferSystem;
+//import org.firstinspires.ftc.teamcode.subsystems.ElevatorSystem;
+//import org.firstinspires.ftc.teamcode.subsystems.TransferSystem;
 
 
 @Config
 @Autonomous(name = "Auto Right Red", group = "auto")
 public class AutoRightRed extends LinearOpMode {
 
-    ElevatorSystem elevator = new ElevatorSystem(hardwareMap);
-    TransferSystem transferSystem;
+ //   ElevatorSystem elevator = new ElevatorSystem(hardwareMap);
+  //  TransferSystem transferSystem;
     //start values.
     public static final double START_POSE_X = 36, START_POSE_Y = -72, START_POSE_ANGLE = 90;
 
     /* NOT FINAL */
-    public static double START_CONE_DELIVERY_X = 36, START_CONE_DELIVERY_Y = -16, START_CONE_DELIVERY_ANGLE = 127;
+    public static double START_CONE_DELIVERY_X = 36, START_CONE_DELIVERY_Y = -12, START_CONE_DELIVERY_ANGLE = 127;
     public static double CONE_DELIVERY_POSE_X = 36, CONE_DELIVERY_POSE_Y = -12, CONE_DELIVERY_POSE_ANGLE = 127;
-    public static double CONE_INTAKE_POSE_X = 56, CONE_INTAKE_POSE_Y = -12, CONE_INTAKE_POSE_ANGLE = 180;
+    public static double CONE_INTAKE_POSE_X = 56, CONE_INTAKE_POSE_Y = -12, CONE_INTAKE_POSE_ANGLE = 0;
+
+    public static double NEAR_CONE_INTAKE_POSE_X = 44, NEAR_CONE_INTAKE_POSE_Y = -12;
     //Need to put actual coordinates
 
     public static final double DELIVERY_WAIT_TIME = 1, INTAKE_WAIT_TIME = 2;
@@ -41,13 +43,17 @@ public class AutoRightRed extends LinearOpMode {
         Pose2d startPose = new Pose2d(START_POSE_X, START_POSE_Y, Math.toRadians(START_POSE_ANGLE));
         drivetrain.setPoseEstimate(startPose);
 
-        Pose2d coneStackPose1 = new Pose2d(56, -12, Math.toRadians(180));
         Pose2d firstCycleBarPose = new Pose2d(START_CONE_DELIVERY_X, START_CONE_DELIVERY_Y, Math.toRadians(START_CONE_DELIVERY_ANGLE));
         Pose2d secondCycleBarPose = new Pose2d(CONE_DELIVERY_POSE_X, CONE_DELIVERY_POSE_Y, Math.toRadians(CONE_DELIVERY_POSE_ANGLE));
+
+        Pose2d nearConeStackPose = new Pose2d(NEAR_CONE_INTAKE_POSE_X, NEAR_CONE_INTAKE_POSE_Y, Math.toRadians(CONE_INTAKE_POSE_ANGLE));
         Pose2d coneStackPose = new Pose2d(CONE_INTAKE_POSE_X, CONE_INTAKE_POSE_Y, Math.toRadians(CONE_INTAKE_POSE_ANGLE));
+
+
         Pose2d coneStackPoseSecond = new Pose2d(CONE_INTAKE_POSE_X + INTAKE_OFFSET, CONE_INTAKE_POSE_Y, Math.toRadians(CONE_INTAKE_POSE_ANGLE));
         Pose2d coneStackPoseThird = new Pose2d(CONE_INTAKE_POSE_X + 2 * INTAKE_OFFSET, CONE_INTAKE_POSE_Y, Math.toRadians(CONE_INTAKE_POSE_ANGLE));
 
+        /*
         MarkerCallback ElevatorMax = new MarkerCallback() {
             @Override
             public void onMarkerReached() {
@@ -56,36 +62,40 @@ public class AutoRightRed extends LinearOpMode {
             }
 
 
-            TrajectorySequence autoCycles = drivetrain.trajectorySequenceBuilder(startPose)
+        };*/
 
-                    /* First Cycle*/
+            /* First Cycle*/
+            TrajectorySequence firstCycle =drivetrain.trajectorySequenceBuilder(startPose)
                     .lineToLinearHeading(firstCycleBarPose)//delivery
                     .waitSeconds(DELIVERY_WAIT_TIME)
+                    .build();
+
+            TrajectorySequence autoCycles = drivetrain.trajectorySequenceBuilder(firstCycle.end())
+
+
 
                     /*Second Cycle*/
-                    //   .splineToLinearHeading(coneStackPose,Math.toRadians(200))//intake
-
-                    .lineToSplineHeading(coneStackPose1)
-                    .splineToLinearHeading(coneStackPose, Math.toRadians(START_POSE_ANGLE))//intake
-
-//                .lineToSplineHeading(coneStackPose)
-                    .waitSeconds(INTAKE_WAIT_TIME)
-
-                    .lineToLinearHeading(secondCycleBarPose)//delivery
-                    .waitSeconds(DELIVERY_WAIT_TIME)
-
-                    /*Third Cycle*/
-                    .splineToLinearHeading(coneStackPose1, Math.toRadians(START_POSE_ANGLE))//intake
-                    .lineToSplineHeading(coneStackPose)
-
-//                .lineToLinearHeading(coneStackPoseSecond)//intake
-                    .waitSeconds(INTAKE_WAIT_TIME)
-
-                    .lineToLinearHeading(secondCycleBarPose)//delivery
-                    .waitSeconds(DELIVERY_WAIT_TIME)
-
+                    .splineToLinearHeading(nearConeStackPose,Math.toRadians(CONE_DELIVERY_POSE_ANGLE))
+                    .lineToLinearHeading(coneStackPose)
                     .build();
-        };
+
+
+
+        if (isStopRequested())
+        {
+                return;
+        }
+            waitForStart();
+        while(opModeIsActive())
+        {
+            drivetrain.followTrajectorySequence(firstCycle);
+            drivetrain.followTrajectorySequence(autoCycles);
+
+        }
+
+
+
+
     }
 }
 
