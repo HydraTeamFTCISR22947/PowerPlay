3b6c6f9a3ad267c06fc5e64970183af5470ca6d0
Elior Yousefi <elior@abilityedge.co.il>
2023-02-11 02:46:02 +0200

    all auto sides should be ok, while right blue being main

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepREDRIGHT.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepREDRIGHT.java
@@ -11,84 +11,91 @@ import com.acmerobotics.roadrunner.trajectory.constraints.TranslationalVelocityC
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-
 import java.util.Arrays;
 
 public class MeepMeepREDRIGHT {
-
-    public static double startPosX = 36, startPosY = -66, startPosAngle = 180;
-    public static double startConeStrafe1 = 58.8, startConeStrafe2 = 20, startConeForward = 4.5;
-    public static double intakePose1X = 35, intakePose1Y = -20.3, intakePose1Angle = 180;
-    public static double intakePose2X = 62, intakePose2Y = -15, intakePose2Angle = 180;
-    public static double intakePoseCycleX = 38;
-    public static double posCone1X = 40, posCone1Y = -15;
-    public static double posCone2X = 32, posCone2Y = -21, posCone2Angle = 225;
-    public static double PARK_ASSIST = 20, TARGET_ZONE = 20;
-
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(550);
 
+        double startPosX = 36, startPosY = -65.9, startPosAngle = 180;
+
+        double BACK_WAIT_TIME = 0.1, DELIVERY_WAIT_TIME = .25, RELEASE_WAIT_TIME = .33;
+        double ALMOST_RELEASE_TIME = 0.1, INTAKE_WAIT_TIME = .1, ELEVATOR_WAIT_TIME = .5;
+
+        double startConeStrafe1 = 54.8, startConeStrafe2 = 16.05, startConeForward = 5.15;
+        double backIntakeOffset  = 13;
+        double intakePose1XFirstCone = 35, intakePose1YFirstCone = -19.8;
+        double intakePose2XFirstCone = 46.7, intakePose2YFirstCone = -15.6;
+        double intakePoseCycleXSecondCone = 38, intakePose1YSecondCone = -15.7;
+        double intakePose2XSecondCone = 50, intakePose2YSecondCone = -15.4;
+        double intakePoseCycleXThirdCone = 38, intakePose1YThirdCone = -15.7;
+        double intakePose2XThirdCone = 51, intakePose2YThirdCone = -15;
+        double intakeAngle = 180;
+        double posCone1X = 40, posCone1Y = -15, posConeAngle = 225;
+        double posCone2XFirstCone = 30.45, posCone2YFirstCone = -18.05;
+        double posCone2XSecondCone = 30.6, posCone2YSecondCone = -17.4;
+        double posCone2XThirdCone = 31.95, posCone2YThirdCone = -17.2;
+        double parkPoseX = 39.3, parkPoseY = -20, parkPoseAngle = 180;
+        double TARGET_ZONE = 20, GO_TO_PARK_HELPER = 8;
         TrajectoryVelocityConstraint velConstraint = new MinVelocityConstraint(Arrays.asList(
                 new TranslationalVelocityConstraint(70), new TranslationalVelocityConstraint(70)));
         TrajectoryAccelerationConstraint accelConstraint = new ProfileAccelerationConstraint(60);
 
-        RoadRunnerBotEntity myBot1 = new DefaultBotBuilder(meepMeep)
-
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
 
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(130), Math.toRadians(130), 13.23)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(startPosX, startPosY, Math.toRadians(startPosAngle)))
+                        drive.trajectorySequenceBuilder(new Pose2d(-startPosX, -startPosY, Math.toRadians(0)))
                                 .strafeRight(startConeStrafe1, velConstraint, accelConstraint)
-                                .strafeLeft(startConeStrafe2)
-                                .forward(startConeForward)
-
-
-                                .waitSeconds(1)
-
-
-                                .lineTo(new Vector2d(intakePose1X, intakePose1Y))
-                                .splineToLinearHeading(new Pose2d(intakePose2X, intakePose2Y, Math.toRadians(intakePose2Angle)), Math.toRadians(0))
-
-
-                                .waitSeconds(1)
-
-
-                                .lineTo(new Vector2d(posCone1X, posCone1Y))
-                                .splineTo(new Vector2d(posCone2X, posCone2Y), Math.toRadians(posCone2Angle))
-
-
+                                .strafeLeft(startConeStrafe2, velConstraint, accelConstraint)
+                                .forward(startConeForward, velConstraint, accelConstraint)
+                                .waitSeconds(DELIVERY_WAIT_TIME)
+                                .waitSeconds(ALMOST_RELEASE_TIME)
+                                .waitSeconds(RELEASE_WAIT_TIME)
+                                .lineTo(new Vector2d(-intakePose1XFirstCone, -intakePose1YFirstCone))
+                                .splineToLinearHeading(new Pose2d(-intakePose2XFirstCone, -intakePose2YFirstCone, Math.toRadians(intakeAngle - 180)), Math.toRadians(0))
+                                .waitSeconds(BACK_WAIT_TIME)
+                                .back(backIntakeOffset)
+                                .waitSeconds(INTAKE_WAIT_TIME)
+                                .waitSeconds(ELEVATOR_WAIT_TIME)
+                                .lineTo(new Vector2d(-posCone1X, -posCone1Y))
+                                .splineTo(new Vector2d(-posCone2XFirstCone, -posCone2YFirstCone), Math.toRadians(-posConeAngle - 90))
+                                .waitSeconds(DELIVERY_WAIT_TIME)
+                                .waitSeconds(ALMOST_RELEASE_TIME)
+                                .waitSeconds(RELEASE_WAIT_TIME)
                                 .setReversed(true)
-
-
-                                .waitSeconds(1)
-
-
-                                .splineToSplineHeading(new Pose2d(intakePoseCycleX, intakePose2Y, Math.toRadians(intakePose1Angle)), Math.toRadians(0))
-                                .lineTo(new Vector2d(intakePose2X, intakePose2Y))
-                                .waitSeconds(1)
-
-                                .setReversed(false)
-
-                                .lineTo(new Vector2d(posCone1X, posCone1Y))
-                                .splineTo(new Vector2d(posCone2X, posCone2Y), Math.toRadians(posCone2Angle))
+                                .splineToSplineHeading(new Pose2d(-intakePoseCycleXSecondCone, -intakePose1YSecondCone, Math.toRadians(intakeAngle - 180)), Math.toRadians(180))
+                                .lineTo(new Vector2d(-intakePose2XSecondCone, -intakePose2YSecondCone))
+                                .waitSeconds(BACK_WAIT_TIME)
+                                .back(backIntakeOffset)
+                                .waitSeconds(INTAKE_WAIT_TIME)
+                                .waitSeconds(ELEVATOR_WAIT_TIME)
+                                .lineTo(new Vector2d(-posCone1X, -posCone1Y))
+                                .splineTo(new Vector2d(-posCone2XSecondCone, -posCone2YSecondCone), Math.toRadians(-posConeAngle - 90))
+                                .waitSeconds(DELIVERY_WAIT_TIME)
+                                .waitSeconds(ALMOST_RELEASE_TIME)
+                                .waitSeconds(RELEASE_WAIT_TIME)
                                 .setReversed(true)
-                                .waitSeconds(1)
-                                .splineToSplineHeading(new Pose2d(intakePoseCycleX, intakePose2Y, Math.toRadians(intakePose1Angle)), Math.toRadians(0))
-                                .lineTo(new Vector2d(intakePose2X, intakePose2Y))
-                                .waitSeconds(1)
-                                .setReversed(false)
-                                .lineTo(new Vector2d(posCone1X, posCone1Y))
-                                .splineTo(new Vector2d(posCone2X, posCone2Y), Math.toRadians(posCone2Angle))
-                                .waitSeconds(1)
-                                .lineToLinearHeading(new Pose2d(intakePose1X, intakePose1Y, Math.toRadians(intakePose1Angle)))
-                                .strafeLeft(PARK_ASSIST)
+                                .splineToSplineHeading(new Pose2d(-intakePoseCycleXThirdCone, -intakePose1YThirdCone, Math.toRadians(intakeAngle - 180)), Math.toRadians(180))
+                                .lineTo(new Vector2d(-intakePose2XThirdCone, -intakePose2YThirdCone))
+                                .waitSeconds(BACK_WAIT_TIME)
+                                .back(backIntakeOffset)
+                                .waitSeconds(INTAKE_WAIT_TIME)
+                                .waitSeconds(ELEVATOR_WAIT_TIME)
+                                .lineTo(new Vector2d(-posCone1X, -posCone1Y))
+                                .splineTo(new Vector2d(-posCone2XThirdCone, -posCone2YThirdCone), Math.toRadians(-posConeAngle - 90))
+                                .waitSeconds(DELIVERY_WAIT_TIME)
+                                .waitSeconds(ALMOST_RELEASE_TIME)
+                                .waitSeconds(RELEASE_WAIT_TIME)
+                                .back(GO_TO_PARK_HELPER,velConstraint,accelConstraint)
+                                .lineToLinearHeading(new Pose2d(-parkPoseX, -parkPoseY, Math.toRadians(0)),velConstraint,accelConstraint)
                                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot1)
+                .addEntity(myBot)
                 .start();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoLeftRed.java
@@ -0,0 +1,336 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TranslationalVelocityConstraint;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.commands.auto.AutoCommands;
+import org.firstinspires.ftc.teamcode.roadrunner.DriveConstants;
+import org.firstinspires.ftc.teamcode.roadrunner.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.subsystems.AprilTagDetectionPipeline;
+import org.firstinspires.ftc.teamcode.subsystems.ClawServo;
+import org.firstinspires.ftc.teamcode.subsystems.ElevatorSystem;
+import org.firstinspires.ftc.teamcode.subsystems.RotationServo;
+import org.firstinspires.ftc.teamcode.subsystems.TransferSystem;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+@Config
+@Autonomous(name = "Auto Right Blue", group = "auto")
+public class AutoLeftRed extends LinearOpMode {
+    public static boolean useCamera = false;
+
+    public static double startPosX = 36, startPosY = 65.9, startPosAngle = 180;
+
+    public static double BACK_WAIT_TIME = 0.1, DELIVERY_WAIT_TIME = .25, RELEASE_WAIT_TIME = .33;
+    public static double ALMOST_RELEASE_TIME = 0.1, INTAKE_WAIT_TIME = .1, ELEVATOR_WAIT_TIME = .5;
+
+    final int ID_TAG_OF_INTEREST1 = 0, ID_TAG_OF_INTEREST2 = 1, ID_TAG_OF_INTEREST3 = 2; // Tags from the 36h11 family
+
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    enum ParkIn
+    {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    ParkIn parkIn = ParkIn.CENTER;
+
+    AutoCommands autoCommands;
+    ClawServo clawServo;
+    ElevatorSystem elevatorSystem;
+    TransferSystem transferSystem;
+    RotationServo rotationServo;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        if(useCamera)
+        {
+            initCamera();
+        }
+
+        SampleMecanumDrive drivetrain = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(startPosX, startPosY, Math.toRadians(startPosAngle));
+        autoCommands = new AutoCommands(hardwareMap);
+        clawServo = new ClawServo(hardwareMap);
+        elevatorSystem = new ElevatorSystem(hardwareMap);
+        transferSystem = new TransferSystem(hardwareMap);
+        rotationServo = new RotationServo(hardwareMap);
+
+
+        drivetrain.setPoseEstimate(startPose);
+
+        TrajectoryVelocityConstraint velConstraint = new MinVelocityConstraint(Arrays.asList(
+                new TranslationalVelocityConstraint(70), new TranslationalVelocityConstraint(70)));
+
+        TrajectoryAccelerationConstraint accelConstraint = new ProfileAccelerationConstraint(DriveConstants.MAX_ACCEL);
+
+        TrajectorySequence preload = drivetrain.trajectorySequenceBuilder(startPose)
+                .strafeLeft(AutoRightBlue.startConeStrafe1, velConstraint, accelConstraint)
+                .strafeRight(AutoRightBlue.startConeStrafe2, velConstraint, accelConstraint)
+                .forward(AutoRightBlue.startConeForward, velConstraint, accelConstraint)
+                .waitSeconds(DELIVERY_WAIT_TIME)
+                .addTemporalMarker(autoCommands.goDownToReleaseCone())
+                .waitSeconds(ALMOST_RELEASE_TIME)
+                .addTemporalMarker(autoCommands.releaseCone())
+                .waitSeconds(RELEASE_WAIT_TIME)
+                .build();
+
+        TrajectorySequence cycle1 = drivetrain.trajectorySequenceBuilder(preload.end())
+                .lineTo(new Vector2d(AutoRightBlue.intakePose1XFirstCone, -AutoRightBlue.intakePose1YFirstCone))
+                .addTemporalMarker(autoCommands.intakeFirstCone())
+                .splineToLinearHeading(new Pose2d(AutoRightBlue.intakePose2XFirstCone, -AutoRightBlue.intakePose2YFirstCone, Math.toRadians(AutoRightBlue.intakeAngle)), Math.toRadians(0))
+                .waitSeconds(BACK_WAIT_TIME)
+                .back(AutoRightBlue.backIntakeOffset)
+                .waitSeconds(INTAKE_WAIT_TIME)
+                .addTemporalMarker(autoCommands.catchCone())
+                .waitSeconds(ELEVATOR_WAIT_TIME)
+                .addTemporalMarker(autoCommands.elevatorIntake())
+                //.waitSeconds(DELIVERY_WAIT_TIME*3)
+                .build();
+
+        TrajectorySequence place1 = drivetrain.trajectorySequenceBuilder(cycle1.end())
+                .lineTo(new Vector2d(AutoRightBlue.posCone1X, -AutoRightBlue.posCone1Y))
+                .addTemporalMarker(autoCommands.readyToRelease())
+                .splineTo(new Vector2d(AutoRightBlue.posCone2XFirstCone, -AutoRightBlue.posCone2YFirstCone), Math.toRadians(AutoRightBlue.posConeAngle + 270))
+                .waitSeconds(DELIVERY_WAIT_TIME)
+                .addTemporalMarker(autoCommands.goDownToReleaseCone())
+                .waitSeconds(ALMOST_RELEASE_TIME)
+                .addTemporalMarker(autoCommands.releaseCone())
+                .waitSeconds(RELEASE_WAIT_TIME)
+                .build();
+
+
+        TrajectorySequence cycle2 = drivetrain.trajectorySequenceBuilder(place1.end())
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(AutoRightBlue.intakePoseCycleXSecondCone, -AutoRightBlue.intakePose1YSecondCone, Math.toRadians(AutoRightBlue.intakeAngle)), Math.toRadians(0))
+                .addTemporalMarker(autoCommands.intakeSecondCone())
+                .lineTo(new Vector2d(AutoRightBlue.intakePose2XSecondCone, -AutoRightBlue.intakePose2YSecondCone))
+                .waitSeconds(BACK_WAIT_TIME)
+                .back(AutoRightBlue.backIntakeOffset)
+                .waitSeconds(INTAKE_WAIT_TIME)
+                .addTemporalMarker(autoCommands.catchCone())
+                .waitSeconds(ELEVATOR_WAIT_TIME)
+                .addTemporalMarker(autoCommands.elevatorIntake())
+                //.waitSeconds(DELIVERY_WAIT_TIME*2)
+                .build();
+
+        TrajectorySequence place2 = drivetrain.trajectorySequenceBuilder(cycle2.end())
+                .lineTo(new Vector2d(AutoRightBlue.posCone1X, -AutoRightBlue.posCone1Y))
+                .addTemporalMarker(autoCommands.readyToRelease())
+                .splineTo(new Vector2d(AutoRightBlue.posCone2XSecondCone, -AutoRightBlue.posCone2YSecondCone), Math.toRadians(AutoRightBlue.posConeAngle + 270))
+                .waitSeconds(DELIVERY_WAIT_TIME)
+                .addTemporalMarker(autoCommands.goDownToReleaseCone())
+                .waitSeconds(ALMOST_RELEASE_TIME)
+                .addTemporalMarker(autoCommands.releaseCone())
+                .waitSeconds(RELEASE_WAIT_TIME)
+                .build();
+
+        TrajectorySequence cycle3 = drivetrain.trajectorySequenceBuilder(place2.end())
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(AutoRightBlue.intakePoseCycleXThirdCone, -AutoRightBlue.intakePose1YThirdCone, Math.toRadians(AutoRightBlue.intakeAngle)), Math.toRadians(0))
+                .addTemporalMarker(autoCommands.intakeThirdCone())
+                .lineTo(new Vector2d(AutoRightBlue.intakePose2XThirdCone, -AutoRightBlue.intakePose2YThirdCone))
+                .waitSeconds(BACK_WAIT_TIME)
+                .back(AutoRightBlue.backIntakeOffset)
+                .waitSeconds(INTAKE_WAIT_TIME)
+                .addTemporalMarker(autoCommands.catchCone())
+                .waitSeconds(ELEVATOR_WAIT_TIME)
+                .addTemporalMarker(autoCommands.elevatorIntake())
+                //.waitSeconds(DELIVERY_WAIT_TIME*2)
+                .build();
+
+        TrajectorySequence place3 = drivetrain.trajectorySequenceBuilder(cycle3.end())
+                .lineTo(new Vector2d(AutoRightBlue.posCone1X, -AutoRightBlue.posCone1Y))
+                .addTemporalMarker(autoCommands.readyToRelease())
+                .splineTo(new Vector2d(AutoRightBlue.posCone2XThirdCone, -AutoRightBlue.posCone2YThirdCone), Math.toRadians(AutoRightBlue.posConeAngle + 270))
+                .waitSeconds(DELIVERY_WAIT_TIME)
+                .addTemporalMarker(autoCommands.goDownToReleaseCone())
+                .waitSeconds(ALMOST_RELEASE_TIME)
+                .addTemporalMarker(autoCommands.releaseCone())
+                .waitSeconds(RELEASE_WAIT_TIME)
+                .build();
+
+
+        TrajectorySequence park1 = drivetrain.trajectorySequenceBuilder(place3.end())
+                .back(AutoRightBlue.GO_TO_PARK_HELPER,velConstraint,accelConstraint)
+                .addTemporalMarker(autoCommands.readyToRelease())
+                .addTemporalMarker(autoCommands.reset())
+                .lineToLinearHeading(new Pose2d(AutoRightBlue.parkPoseX, -AutoRightBlue.parkPoseY, Math.toRadians(0)),velConstraint,accelConstraint)
+                .forward(AutoRightBlue.TARGET_ZONE)
+                .build();
+
+        TrajectorySequence park2 = drivetrain.trajectorySequenceBuilder(place3.end())
+                .back(AutoRightBlue.GO_TO_PARK_HELPER,velConstraint,accelConstraint)
+                .addTemporalMarker(autoCommands.readyToRelease())
+                .addTemporalMarker(autoCommands.reset())
+                .lineToLinearHeading(new Pose2d(AutoRightBlue.parkPoseX, -AutoRightBlue.parkPoseY, Math.toRadians(0)),velConstraint,accelConstraint)
+                .build();
+
+        TrajectorySequence park3 = drivetrain.trajectorySequenceBuilder(place3.end())
+                .back(AutoRightBlue.GO_TO_PARK_HELPER,velConstraint,accelConstraint)
+                .addTemporalMarker(autoCommands.readyToRelease())
+                .addTemporalMarker(autoCommands.reset())
+                .lineToLinearHeading(new Pose2d(AutoRightBlue.parkPoseX, -AutoRightBlue.parkPoseY, Math.toRadians(0)),velConstraint,accelConstraint)
+                .back(AutoRightBlue.TARGET_ZONE)
+                .build();
+
+        if (isStopRequested()) {return;}
+
+        clawServo.closeClaw();
+
+        if(useCamera)
+        {
+            lookWhereToPark();
+        }
+
+        waitForStart();
+
+        elevatorSystem.goToPos(elevatorSystem.BASE_HEIGHT);
+        elevatorSystem.midRod();
+        rotationServo.pickUpPos();
+        transferSystem.highPos();
+
+        drivetrain.followTrajectorySequence(preload);
+
+        drivetrain.followTrajectorySequence(cycle1);
+        drivetrain.followTrajectorySequence(place1);
+
+        drivetrain.followTrajectorySequence(cycle2);
+        drivetrain.followTrajectorySequence(place2);
+
+        drivetrain.followTrajectorySequence(cycle3);
+        drivetrain.followTrajectorySequence(place3);
+
+        if(useCamera)
+        {
+            switch (parkIn)
+            {
+                case LEFT:
+                    drivetrain.followTrajectorySequence(park1);
+                    break;
+                case RIGHT:
+                    drivetrain.followTrajectorySequence(park3);
+                    break;
+                case CENTER:
+                default:
+                    drivetrain.followTrajectorySequence(park2);
+                    break;
+            }
+        }
+        else
+        {
+            drivetrain.followTrajectorySequence(park2);
+        }
+
+        while (opModeIsActive());
+    }
+
+    void lookWhereToPark()
+    {
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    switch (tag.id)
+                    {
+                        case ID_TAG_OF_INTEREST1:
+                        default:
+                            parkIn = ParkIn.CENTER;
+                            break;
+                        case ID_TAG_OF_INTEREST2:
+                            parkIn = ParkIn.RIGHT;
+                            break;
+                        case ID_TAG_OF_INTEREST3:
+                            parkIn = ParkIn.LEFT;
+                            break;
+                    }
+                }
+
+                switch (parkIn)
+                {
+                    case LEFT:
+                        telemetry.addLine("Parking In Left Zone!");
+                        break;
+                    case CENTER:
+                        telemetry.addLine("Parking In Center Zone(May not see tag)!");
+                        break;
+                    case RIGHT:
+                        telemetry.addLine("Parking In Right Zone!");
+                        break;
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+        }
+    }
+
+    void initCamera()
+    {
+        // Lens intrinsics
+        // UNITS ARE PIXELS
+        // NOTE: this calibration is for the C920 webcam at 800x448.
+        // You will need to do your own calibration for other configurations!
+        double fx = 578.272;
+        double fy = 578.272;
+        double cx = 402.145;
+        double cy = 221.506;
+
+        // UNITS ARE METERS
+        double tagsize = 0.166;
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+    }
+
+}
