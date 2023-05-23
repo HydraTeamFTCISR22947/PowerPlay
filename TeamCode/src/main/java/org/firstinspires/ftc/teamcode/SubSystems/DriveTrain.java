c2517281f30cd097feae55a75c9f6d1e08f3ce9b
noampima <104015037+noampima@users.noreply.github.com>
2023-05-23 23:21:35 +0300

    Create DriveTrain.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/DriveTrain.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import android.graphics.drawable.GradientDrawable;
+
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.checkerframework.checker.units.qual.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class DriveTrain
+{
+    IMU imu;
+    double offset = Math.PI + Math.PI /2; //offset to know if it's red or blue (the angle of robot), when we with red
+
+
+    //all the motors:
+    DcMotor front_left = null;
+    DcMotor front_right = null;
+    DcMotor back_right = null;
+    DcMotor back_left = null;
+
+    //ctor
+    public DriveTrain(HardwareMap hardwareMap, boolean red)
+    {
+        //setting all motors:
+        front_left = hardwareMap.get(DcMotor.class, "mFL");
+        front_right = hardwareMap.get(DcMotor.class, "mFR");
+        back_right = hardwareMap.get(DcMotor.class, "mBR");
+        back_left = hardwareMap.get(DcMotor.class, "mBL");
+        imu = hardwareMap.get(IMU.class, "imu");
+
+
+        //setting the left side as reversed
+        front_left.setDirection(DcMotorSimple.Direction.REVERSE);
+        back_left.setDirection(DcMotorSimple.Direction.REVERSE);
+        front_right.setDirection(DcMotorSimple.Direction.FORWARD);
+        back_right.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        //setting the power of all motors (init)
+        front_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        back_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        front_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        back_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+        if (!red)
+            offset = -offset;
+
+    }
+
+
+
+    public void update(Gamepad gamepad1)
+    {
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+        // This button choice was made so that it is hard to hit on accident,
+        // it can be freely changed based on preference.
+        // The equivalent button is start on Xbox-style controllers.
+        if (gamepad1.options) {
+            imu.resetYaw();
+        }
+
+        //double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS) + offset; Elior isn't sure
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        // Rotate the movement direction counter to the bot's rotation
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        front_left.setPower(frontLeftPower);
+        back_left.setPower(backLeftPower);
+        front_right.setPower(frontRightPower);
+        back_right.setPower(backRightPower);
+    }
+
+
+}
