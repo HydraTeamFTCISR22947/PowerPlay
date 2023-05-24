624219b1570d7d141b5fe714cd3eec58d38aa6c8
noampima <104015037+noampima@users.noreply.github.com>
2023-05-24 19:32:45 +0300

    Create JunctionGuide.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/JunctionGuide.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.sun.tools.javac.tree.DCTree;
+
+public class JunctionGuide {
+    Servo servo = null;
+
+    // position constants
+    public static double DOWN = 0;
+    public static double UP = 0.1667;
+
+    // JunctionGuide constructor
+    public JunctionGuide(HardwareMap map)
+    {
+        this.servo = map.get(Servo.class, "servo_guide");
+    }
+
+    // move guide up
+    public void moveUp()
+    {
+        this.servo.setPosition(UP);
+    }
+
+    // move guide down
+    public void moveDown()
+    {
+        this.servo.setPosition(DOWN);
+    }
+
+
+}
