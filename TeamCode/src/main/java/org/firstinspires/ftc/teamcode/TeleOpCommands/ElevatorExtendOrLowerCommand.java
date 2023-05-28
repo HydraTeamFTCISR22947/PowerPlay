ad6c20a38857df31052797384d51d959c5227009
noampima <104015037+noampima@users.noreply.github.com>
2023-05-28 17:56:18 +0300

    mmmmm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCommands/ElevatorExtendOrLowerCommand.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.TeleOpCommands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.Subsystem;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.robocol.Command;
+
+import org.firstinspires.ftc.teamcode.ElevatorCode;
+import org.firstinspires.ftc.teamcode.SubSystems.ElevatorSystem;
+
+public class ElevatorExtendOrLowerCommand extends CommandBase {
+
+    public ElevatorSystem elevatorSystem;
+
+    HardwareMap hW;
+
+
+
+    public ElevatorExtendOrLowerCommand (ElevatorSystem elevatorSystem , HardwareMap hardwareMap) {
+
+        this.elevatorSystem = elevatorSystem;
+        this.hW = hardwareMap;
+
+        addRequirements((Subsystem) elevatorSystem);
+    }
+
+
+    @Override
+    public void initialize() {
+
+        elevatorSystem.baseLevel();
+
+    }
+
+    public void execute(){
+
+        if()
+
+
+    }
+
+
+
+}
