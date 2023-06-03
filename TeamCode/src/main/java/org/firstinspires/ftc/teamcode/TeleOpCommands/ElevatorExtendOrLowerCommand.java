01ceb9696596757bd1d5fa94147d3dbc7654f9d2
noampima <104015037+noampima@users.noreply.github.com>
2023-06-04 00:22:39 +0300

    Revert "k"
    This reverts commit 83639de3fa48fac4fb1e9a43ffba9229168b869f.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCommands/ElevatorExtendOrLowerCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpCommands/ElevatorExtendOrLowerCommand.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.SubSystems.ElevatorSystem;
 
 public class ElevatorExtendOrLowerCommand extends CommandBase {
 
-    public elevator elevatorSystem;
+    public ElevatorSystem elevatorSystem;
 
     HardwareMap hW;
 
